593769e395dfec1381de6edc06f2542d64088cff
Meet2206 <153612292+Meet2206@users.noreply.github.com>
2025-05-06 16:20:26 +0530

    Updated with og images

--- src/data/stayData.ts
+++ src/data/stayData.ts
@@ -17,7 +17,7 @@ export const stayOptions: StayOption[] = [
     price: '₹2,500',
     features: ['Breakfast Included', 'Room for 4', 'Lake View'],
     image: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/635737785.jpg?k=d5c3f33c42f4ab4dd8e163dabebf88049159747e36039cd40c258cf370cd153d&o=&hp=1',
-    bookingUrl: 'https://www.booking.com'
+    bookingUrl: 'https://www.booking.com/'
   },
   {
     id: 's3',
@@ -25,8 +25,8 @@ export const stayOptions: StayOption[] = [
     location: 'Udaipur',
     price: '₹3,000',
     features: ['Homestay Experience', 'Authentic Food', 'Cultural Ambience'],
-    image: 'https://images.pexels.com/photos/1838640/pexels-photo-1838640.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    bookingUrl: 'https://www.booking.com'
+    image: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/83851381.jpg?k=143cd9932565a130856d44bb7af98fcb27f7756fd0a112a0160648de70dc7441&o=',
+    bookingUrl: 'https://www.booking.com/'
   },
   {
     id: 's4',
@@ -34,8 +34,8 @@ export const stayOptions: StayOption[] = [
     location: 'Udaipur',
     price: '₹1,500',
     features: ['Budget Friendly', 'Dormitory Style', 'Social Atmosphere'],
-    image: 'https://images.pexels.com/photos/271618/pexels-photo-271618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    bookingUrl: 'https://www.booking.com'
+    image: 'https://cf.bstatic.com/xdata/images/hotel/max500/642463766.jpg?k=e3e575cb0cb6d3645257830268377986591fad5445044bdd78ab9b2980828274&o=',
+    bookingUrl: 'https://www.booking.com/'
   },
   {
     id: 's5',
@@ -43,8 +43,8 @@ export const stayOptions: StayOption[] = [
     location: 'Udaipur',
     price: '₹1,300',
     features: ['Budget Option', 'City Views', 'Rooftop Restaurant'],
-    image: 'https://images.pexels.com/photos/6186815/pexels-photo-6186815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    bookingUrl: 'https://www.booking.com'
+    image: 'https://cf.bstatic.com/xdata/images/hotel/max300/534182538.jpg?k=7aa7707adcea829749367f2b4f6c408299a2b87cd44d2e247fb8e55a06740b10&o=',
+    bookingUrl: 'https://www.booking.com/'
   },
   {
     id: 's6',
@@ -52,8 +52,8 @@ export const stayOptions: StayOption[] = [
     location: 'Lake Pichola, Udaipur',
     price: '₹1,000',
     features: ['Lake View', 'Budget Option', 'Traditional Architecture'],
-    image: 'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    bookingUrl: 'https://www.booking.com'
+    image: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/482269886.jpg?k=dedd2dea6a7dab27c75571b089e8710b7eab8520f13c96482403a99d0e1bcc9d&o=',
+    bookingUrl: 'https://www.booking.com/'
   },
   {
     id: 's7',
@@ -61,7 +61,16 @@ export const stayOptions: StayOption[] = [
     location: 'Udaipur',
     price: '₹1,600',
     features: ['Modern Amenities', 'Central Location', 'Restaurant On-site'],
-    image: 'https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    bookingUrl: 'https://www.booking.com'
+    image: 'https://cf.bstatic.com/xdata/images/hotel/max300/526621341.jpg?k=f774e15c67e13ebe6b7e8b634d6b3c3d417f29eebb85e0a1562a5b847884e8a5&o=',
+    bookingUrl: 'https://www.booking.com/'
+  },
+  {
+    id: 's7',
+    name: 'Zostel Udaipur',
+    location: 'Udaipur',
+    price: '₹2,700',
+    features: ['Modern Amenities', 'Comfortable Accommodation:', 'Essential Facilities'],
+    image: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/636106962.jpg?k=f23b8d1162594f67eb4b8f0a3d5a9676643e1d071d1aa6559b14d8471e952683&o=&hp=1',
+    bookingUrl: 'https://www.booking.com/'
   }
 ];
